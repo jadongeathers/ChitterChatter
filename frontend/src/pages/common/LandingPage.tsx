ddcc9ec0fcc7dee21193350aef13a71fccc1bf2f
jadongeathers <jadongeathers@gmail.com>
2025-10-20 16:07:29 -0400

    Updated landing page language

--- frontend/src/pages/common/LandingPage.tsx
+++ frontend/src/pages/common/LandingPage.tsx
@@ -70,7 +70,7 @@ const LandingPage = () => {
               ChitterChatter
             </h1>
             <p className="text-xl md:text-2xl mb-8 max-w-3xl mx-auto text-gray-600">
-            Transform foreign language classroom concepts into authentic, spoken conversations with virtual practice partners
+            Transform language classroom concepts into authentic, spoken conversations with virtual practice partners
             </p>
             <div className="flex flex-col sm:flex-row gap-4 justify-center">
               <Button
